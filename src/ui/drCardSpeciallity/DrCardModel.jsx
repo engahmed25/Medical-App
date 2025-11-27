e9e9e4aaad0b6bc6a5c2fe5a19a48c76e0a283b4
maifarah <maifarah423@gmail.com>
2025-11-27 22:02:56 +0200

    Add heading and description to drCard section

--- src/ui/drCardSpeciallity/DrCardModel.jsx
+++ src/ui/drCardSpeciallity/DrCardModel.jsx
@@ -1,15 +1,17 @@
 function DrCardModel({ img, drSpeciallityTitle }) {
   return (
     <div
-      className="border-2 
-      border-blue-600 
-      flex flex-col 
-      items-center 
-      p-2 
-      transition-transform 
-      duration-300 
-      transform 
-      hover:scale-110"
+      className="border-2
+        border-blue-600 
+        flex flex-col 
+        items-center 
+        p-2 
+        shadow-[0px_5px_15px_rgba(0,0,0,0.35)]
+        rounded-(--main-radius)
+        transition-transform 
+        duration-300 
+        transform 
+        hover:scale-110"
     >
       <img
         src={img}
