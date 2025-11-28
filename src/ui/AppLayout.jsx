6cdada811eee158000e8145877cdd40c5bfa7212
engahmed25 <eng.ahmed.abdelmoneim.1@gmail.com>
2025-11-28 13:54:44 +0200

    add search bar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer";
-
+import Search from "./Search";
 function AppLayout() {
   return (
     <div>
       <h1>App Layout</h1>
       <main>
         <Outlet />
+        <Search />
       </main>
       <Footer />
+      
     </div>
   );
 }
