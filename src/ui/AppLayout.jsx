f3d32a39098931968986facf1b69a957641ce08b
engahmed25 <eng.ahmed.abdelmoneim.1@gmail.com>
2025-11-30 00:42:20 +0200

    create doctor card

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./footer";
 import Search from "./Search";
+import DoctorCard from "./DoctorCard";
+import DoctorList from "./DoctorList";
 function AppLayout() {
   return (
     <div>
@@ -8,6 +10,8 @@ function AppLayout() {
       <main>
         <Outlet />
         <Search />
+        <DoctorCard name="John Doe" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos." specialization="Cardiology" price="100" status="Available" image="https://via.placeholder.com/150" />
+        <DoctorList />
       </main>
       <Footer />
       
